e1d32b2e7d476c1a5609bb34dfe6c9c313400235
HectorShaw <ogawde7@gmail.com>
2024-10-07 00:51:05 +0530

    License updated

--- be/src/Game.ts
+++ be/src/Game.ts
@@ -12,20 +12,19 @@ import { Chess } from 'chess.js'
 export class Game {
     public player1: WebSocket
     public player2: WebSocket
-    private board: string
-    private moves: string[]
+    public board: Chess
     private startTime: Date;
-
 
     constructor(player1: WebSocket, player2: WebSocket) {
         this.player1    = player1;
         this.player2 = player2;
-        this.board = ""
-        this.moves = []
+        this.board = new Chess()
         this.startTime = new Date()
 
     }
 
     makeMove(socket: WebSocket, move: string) {
+        //validation of your moves
+
     } 
 }   
